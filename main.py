77d97d2ffc006b01ade3204a256b28373d46799f
Demo User <demouser@MacBook-Pro.local>
2018-07-31 10:19:50 -0500

    changed everything to appengine format

--- main.py
+++ main.py
@@ -14,19 +14,39 @@ jinja_env = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        start_template = jinja_env.get_template("mainpage.html")
+        start_template = jinja_env.get_template("templates/mainpage.html")
         self.response.write(start_template.render())
 
 class BottleHandler(webapp2.RequestHandler):
     def get(self):
-        bottle_template = jinja_env.get_template("glassbottle.html")
+        bottle_template = jinja_env.get_template("templates/glassbottle.html")
         self.response.write(bottle_template.render())
 
+    def post(self):
+        text = self.request.get("entry")
+        
 class LoginHandler(webapp2.RequestHandler):
     def post(self):
         login_template = jinja_env.get_template("Login/templates/login.html")
 
+class ResourceHandler(webapp2.RequestHandler):
+    def get(self):
+        resources_template = jinja_env.get_template("templates/resources.html")
+
+class MusicHandler(webapp2.RequestHandler):
+    def get(self):
+        music_template = jinja_env.get_template("templates/music.html")
+
+class AboutHandler(webapp2.RequestHandler):
+    def get(self):
+        about_template = jinja_env.get_template("templates/about.html")
+
 app = webapp2.WSGIApplication([
     ("/", MainHandler),
-    ("/login", LoginHandler)
+    ("/glass-bottle", BottleHandler),
+    ("/login", LoginHandler),
+    ("/resources", ResourceHandler),
+    ("/music", MusicHandler),
+    ("/about", AboutHandler)
+
 ], debug=True)
