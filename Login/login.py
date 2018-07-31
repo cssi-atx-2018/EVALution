be0c6b992b64ccdf832a2e647d840869a1920913
Demo User <demouser@MacBook-Pro.local>
2018-07-31 16:10:11 -0500

    the failed experiment

--- Login/login.py
+++ Login/login.py
@@ -11,13 +11,15 @@ class input(webapp2.RequestHandler):
         user = users.get_current_user()
         if user:
             nickname = user.nickname()
+            self.response.write("you can continue")
             logout_url = users.create_logout_url('/')
             greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
             nickname, logout_url)
         else:
             login_url = users.create_login_url('/')
-             greeting = '<a href="{}">Sign in</a>'.format(login_url)
-  self.response.write('<html><body>{}</body></html>'.format(greeting))
+            self.response.write('You must login')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+        self.response.write('<html><body>{}</body></html>'.format(greeting))
 
 
 
