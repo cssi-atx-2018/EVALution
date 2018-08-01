026a7961eeb984b48624216de7cf01fa9525770a
lilliansun <lillys.6538@gmail.com>
2018-08-01 09:49:23 -0500

    Delete button.js

--- button.js
+++
@@ -1,6 +0,0 @@
-'''This javascript file will select the post and delete buttons on the main page and manage actions'''
-
-
-/*insert the name of the post and delete button later*/
-postButton = document.querySelector("")
-deleteButton = document.querySelector("")
