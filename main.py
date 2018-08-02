21f576fdcf157c8b97c2746085cd732d30ee1642
Demo User <demouser@MacBook-Pro.local>
2018-08-02 11:43:08 -0500

    login kind of works

--- main.py
+++ main.py
@@ -19,13 +19,16 @@ login_url = users.create_login_url('/')
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        glassbottle_url = "/glass-bottle"
+        if(user):
+            glassbottle_url = "/glass-bottle-lg"
         start_template = jinja_env.get_template("templates/mainpage.html")
-        self.response.write(start_template.render({"login_url": login_url, "user": user}))
+        self.response.write(start_template.render({"login_url": login_url, "logout_url": logout_url}))
 
 class LGBottleHandler(webapp2.RequestHandler):
     def get(self):
         bottle_template = jinja_env.get_template("templates/glassbottlelg.html")
-        self.response.write(bottle_template.render())
+        self.response.write(bottle_template.render({"login_url": login_url, "logout_url": logout_url}))
 
     def post(self):
         text = self.request.get("entry")
@@ -35,20 +38,20 @@ class LGBottleHandler(webapp2.RequestHandler):
 class BottleHandler(webapp2.RequestHandler):
     def get(self):
         bottle_template = jinja_env.get_template("templates/glassbottle.html")
-        self.response.write(bottle_template.render())
+        self.response.write(bottle_template.render({"login_url": login_url, "logout_url": logout_url}))
 # need to fix so that it works only if you are logged in
     def post(self):
         post_template = jinja_env.get_template("templates/posts.html")
         text = self.request.get("entry")
         post = Post(post_content=text, post_user_id="")
         post.put()
-        self.response.write(post_template.render())
+        self.response.write(post_template.render({"login_url": login_url, "logout_url": logout_url}))
 
 class PostHandler(webapp2.RequestHandler):
     def get(self):
         posts = Post.query().fetch()
         post_template = jinja_env.get_template("templates/posts.html")
-        self.response.write(post_template.render({"posts": posts}))
+        self.response.write(post_template.render({"login_url": login_url, "logout_url": logout_url}))
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
@@ -67,17 +70,17 @@ class LoginHandler(webapp2.RequestHandler):
 class ResourceHandler(webapp2.RequestHandler):
     def get(self):
         resources_template = jinja_env.get_template("templates/resources.html")
-        self.response.write(resources_template.render())
+        self.response.write(resources_template.render({"login_url": login_url, "logout_url": logout_url}))
 
 class MusicHandler(webapp2.RequestHandler):
     def get(self):
         music_template = jinja_env.get_template("templates/music.html")
-        self.response.write(music_template.render())
+        self.response.write(music_template.render({"login_url": login_url, "logout_url": logout_url}))
 
 class AboutHandler(webapp2.RequestHandler):
     def get(self):
         about_template = jinja_env.get_template("templates/about.html")
-        self.response.write(about_template.render())
+        self.response.write(about_template.render({"login_url": login_url, "logout_url": logout_url}))
 
 app = webapp2.WSGIApplication([
     ("/", MainHandler),
